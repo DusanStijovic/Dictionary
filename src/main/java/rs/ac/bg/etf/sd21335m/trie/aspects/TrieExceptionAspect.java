675b24cc1177c07b2331d657c939c30c016dde27
Dušan Stijović <dusan.stijovic@rcub.bg.ac.rs>
2023-04-18 20:25:14 +0200

    Little cleanup

--- src/main/java/rs/ac/bg/etf/sd21335m/trie/aspects/TrieExceptionAspect.java
+++ src/main/java/rs/ac/bg/etf/sd21335m/trie/aspects/TrieExceptionAspect.java
@@ -1,15 +1,11 @@
 package rs.ac.bg.etf.sd21335m.trie.aspects;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.junit.jupiter.api.Order;
 import rs.ac.bg.etf.sd21335m.trie.aspects.util.EmailConfig;
 import rs.ac.bg.etf.sd21335m.trie.aspects.util.EmailSender;
 import rs.ac.bg.etf.sd21335m.trie.aspects.util.ExceptionFormatter;
-
-import java.util.logging.Logger;
-
 @Aspect
 @Order(1)
 public class TrieExceptionAspect {
@@ -19,7 +15,7 @@ public class TrieExceptionAspect {
     private Throwable lastThrowable = null;
 
     @AfterThrowing(pointcut = "execution (* rs.ac.bg.etf.sd21335m.trie..*(..))", throwing = "exception")
-    public void logException(JoinPoint joinPoint, Throwable exception) {
+    public void logException(Throwable exception) {
         if (lastThrowable != exception) {
             lastThrowable = exception;
             emailSender.sendEmail(EmailConfig.EMAIL_RECEIVER_EMAIL, "[Exception] Exception happened in trie app", exceptionFormatter.getStackTraceAsString(exception));
--- src/test/java/rs/ac/bg/etf/sd21335m/trie/aspects/TrieLoggingAspectTest.java
+++ src/test/java/rs/ac/bg/etf/sd21335m/trie/aspects/TrieLoggingAspectTest.java
@@ -1,8 +1,6 @@
 package rs.ac.bg.etf.sd21335m.trie.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -11,14 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import rs.ac.bg.etf.sd21335m.trie.match_strategy.MatchStrategy;
 import rs.ac.bg.etf.sd21335m.trie.match_strategy.PrefixMatchStrategy;
 
-import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.List;
-import java.util.logging.Logger;
-
-import static org.mockito.ArgumentMatchers.matches;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class TrieLoggingAspectTest {
@@ -26,8 +18,6 @@ class TrieLoggingAspectTest {
     private final TrieLoggingAspect aspect = new TrieLoggingAspect();
     @Mock
     private JoinPoint joinPoint;
-    @Mock
-    private Logger logger;
 
     @Test
     void logSearchedWords() {
